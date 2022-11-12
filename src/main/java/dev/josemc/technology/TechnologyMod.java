7e2257a7194154b43e01c2f7f67eaa6bc9db7cae
josemc <josemoncab2001@gmail.com>
2022-11-12 11:59:56 +0100

    Initial commit

---
+++ src/main/java/dev/josemc/technology/TechnologyMod.java
@@ -0,0 +1,15 @@
+package dev.josemc.technology;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TechnologyMod implements ModInitializer {
+	public static final String MOD_ID = "technology";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/dev/josemc/technology/TechnologyModClient.java
@@ -0,0 +1,10 @@
+package dev.josemc.technology;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TechnologyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
